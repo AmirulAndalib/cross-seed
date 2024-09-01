e202c884c127bb0b7c40a65893d99d1f9c4669ba
ShanaryS <shanarys@gmail.com>
2024-09-01 11:33:29 -0400

    use result.orElse()

--- src/inject.ts
+++ src/inject.ts
@@ -248,9 +248,7 @@ async function injectFromStalledTorrent({
 			decision,
 			{ onlyCompleted: false },
 		);
-		const linkResult = linkedFilesRootResult.isOk()
-			? linkedFilesRootResult.unwrap()
-			: null;
+		const linkResult = linkedFilesRootResult.orElse(null);
 		if (linkResult && linkResult.linkedNewFiles) {
 			linkedNewFiles = true;
 		}
@@ -335,7 +333,7 @@ async function injectionAlreadyExists({
 	filePathLog,
 }: InjectionAftermath) {
 	const result = await getClient()!.isTorrentComplete(meta.infoHash);
-	let isComplete = result.isOk() ? result.unwrap() : false;
+	let isComplete = result.orElse(false);
 	const anyFullMatch = matches.some(
 		(m) =>
 			m.decision === Decision.MATCH ||
